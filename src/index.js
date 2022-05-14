6b1c680b29c1a25e3a7087156f10d98bf402fcf2
AmanQureshi2056 <aman.rezolut@gmail.com>
2022-05-14 12:40:01 +0530

    added sample text

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const RezList = ({ text }) => {
-  return <div className={styles.test}>This is  list {text}</div>
+  return <div className={styles.test}>This is a sample {text}</div>
 }
